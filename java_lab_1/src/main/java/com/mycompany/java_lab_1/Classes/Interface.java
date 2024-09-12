06148fd62af02d2692504636ab92f69a98475a15
Sergey Ermakov <ermakoffsergey@gmail.com>
2024-09-12 21:45:17 +0300

    Classes for realization

---
+++ java_lab_1/src/main/java/com/mycompany/java_lab_1/Classes/Interface.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.java_lab_1.Classes;
+
+/**
+ *
+ * @author ermak
+ */
+public class Interface {
+    
+}
