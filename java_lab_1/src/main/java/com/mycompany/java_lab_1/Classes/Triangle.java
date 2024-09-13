cc7bb4c47d91f61bf07ed09798379dfe959d4e38
Vlad_P <vlad.petrovskiy00@mail.ru>
2024-09-13 19:30:47 +0300

    triangleMerging

--- java_lab_1/src/main/java/com/mycompany/java_lab_1/Classes/Triangle.java
+++ java_lab_1/src/main/java/com/mycompany/java_lab_1/Classes/Triangle.java
@@ -5,33 +5,33 @@ package com.mycompany.java_lab_1.Classes;
  * @author ermak
  */
 public class Triangle {
-    private IntNum [][]_values;
+    private IntNum _values [][];
     private String strTriangle;
     static final int _default_len = 2;
     static final int _unit = 1;
     
     
     Triangle(IntNum len){
-        _values = new IntNum[len][];
-        for (int n = 0; n < len; n++)
+        _values = new IntNum[len.getValue()][];
+        for (int n = 0; n < len.getValue(); n++)
         {
             int prev = _unit;
-            strTriangle+=prev.getValue();
-            triangle[n] = new int[n+1];
-            triangle[n][0] = IntNum(_unit);
+            strTriangle += prev;
+            _values[n] = new IntNum[n+1];
+            _values[n][0] = new IntNum(_unit);
             for (int k = 1; k <= n; k++)
             {
                 prev *= (n - k + 1); 
                 prev /= k;
-                triangle[n][k] = IntNum(prev);
-                strTriangle+=prev.getValue();
+                _values[n][k] = new IntNum(prev);
+                strTriangle += prev;
             }
-            strTriangle+='\n';
+            strTriangle += '\n';
         }
     }
     
 //    Triangle(){
-//        _values = new IntNum[_default_len][];
+//        _values = new IntNum[_default_len.getValue()][];
 //        _values[0] = new IntNum[_unit];
 //        _values[0][0] = new IntNum(_unit);
 //        _values[1] = new IntNum[_unit];
