a303a7c7e56d20875ac22baf4e88601c46e54c71
Vlad_P <vlad.petrovskiy00@mail.ru>
2024-09-13 08:51:28 +0300

    test

---
+++ Java_labR_1/src/java_labr_1/Java_labR_1.java
@@ -0,0 +1,15 @@
+
+package java_labr_1;
+
+
+public class Java_labR_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Hello world
+    }
+    
+}
