6b8e3f892a85cbf6f9b0c7c143b084781b65babc
Vlad_P <vlad.petrovskiy00@mail.ru>
2024-09-13 09:06:41 +0300

    test4

--- java_lab_1/src/main/java/com/mycompany/java_lab_1/Java_lab_1.java
+++ java_lab_1/src/main/java/com/mycompany/java_lab_1/Java_lab_1.java
@@ -13,6 +13,7 @@ public class Java_lab_1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        // OOOOOOOOOOOOO
     }
 }
 
